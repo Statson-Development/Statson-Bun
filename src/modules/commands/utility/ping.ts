34e17a60f1716638c43dc757ccb6965564dccbdc
Benzo-Fury <neohaggard07@gmail.com>
2024-03-28 16:57:11 +1000

    feat: improved ping cmd

--- src/modules/commands/utility/ping.ts
+++ src/modules/commands/utility/ping.ts
@@ -1,9 +1,25 @@
+import EmbedBuilder from "#utility/templates/embeds/default";
+import { ApplicationCommandType } from "discord.js";
 import { commandModule } from "neos-handler";
 
 export default commandModule({
   name: "ping",
-  description: "A simple ping command.",
+  description: "A simple ping command 🏓.",
+  type: ApplicationCommandType.ChatInput,
   execute: async (interaction) => {
-    await interaction.reply("Pong!");
+    const num = Math.floor(Math.random() * 50);
+
+    const embed = new EmbedBuilder(interaction.member);
+    embed.setDescription(`**Pong 🏓!** - \`${interaction.client.ws.ping}ms\``);
+
+    if (num === 25) {
+      await interaction.reply(
+        "https://tenor.com/view/chiro-kadasj-kadasj-chelle-ping-pong-lets-play-gif-17306125"
+      );
+    } else {
+      await interaction.reply({
+        embeds: [embed],
+      });
+    }
   },
 });
