376e549c2b42e293c1901010a5bdb5e138d5b8f7
Benzo-Fury <neohaggard07@gmail.com>
2024-03-28 16:45:44 +1000

    refractor: improved logging

--- src/utility/wrappers/discord/loggers.ts
+++ src/utility/wrappers/discord/loggers.ts
@@ -1,22 +1,62 @@
-import type { GuildTextBasedChannel, MessageCreateOptions } from "discord.js";
 import { Bot } from "neos-handler";
+import type {
+  Channel,
+  Message,
+  MessageEditOptions,
+  MessageCreateOptions,
+  GuildTextBasedChannel,
+} from "discord.js";
 
-export enum LogLocation {
-    STAFF = "1197408858390401051",
-    INFRACTIONS = "1197408858809839685"
+async function log(locationId: string, options: MessageCreateOptions) {
+  // Resolving client.
+  const client = await Bot.getInstance().container.resolve("@internal/client");
+
+  // Getting the channel to send to.
+  let channel = (await client.channels.fetch(
+    locationId
+  )) as GuildTextBasedChannel;
+
+  return channel.send(options);
 }
 
-async function log(location: LogLocation, options: MessageCreateOptions) {
-    // Resolving client.
-    const client = await Bot.getInstance().container.resolve("@internal/client");
+async function editLog(
+  channel: Channel | string,
+  message: Message | string,
+  editOptions: MessageEditOptions
+) {
+  // Resolving client.
+  const client = await Bot.getInstance().container.resolve("@internal/client");
 
-    // Getting the channel to send to.
-    let channel = await client.channels.fetch(location) as GuildTextBasedChannel;
+  try {
+    // Ensuring channel is resolved.
+    if (typeof channel === "string") {
+      const resolvedChannel = await client.channels.fetch(channel);
 
-    return channel.send(options);
+      if (!resolvedChannel) {
+        throw new Error("Cannot resolve channel.");
+      }
+
+      if (!resolvedChannel.isTextBased()) {
+        throw new Error("Channel is not text based.");
+      }
+
+      channel = resolvedChannel;
+    }
+
+    // Ensuring message is resolved.
+    if (typeof message === "string") {
+      message = await (channel as GuildTextBasedChannel).messages.fetch(
+        message
+      );
+    }
+  } catch (e) {
+    throw new Error(`Error resolving channel or message: ${e}`);
+  }
+
+  return message.edit(editOptions);
 }
 
 export default {
-    LogLocation,
-    log
-}
+  log,
+  editLog,
+};
